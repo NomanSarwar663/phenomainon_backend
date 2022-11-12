c81b7b4db435baa627080ff9edbde30c9ccfac19
Ahsan-khan41 <79780732+Ahsan-khan41@users.noreply.github.com>
2022-11-12 12:02:41 +0500

    make it better

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,10 +4,10 @@ const { requireSignin } = require('../middleware/authorization');
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
-router.post("/verify-email", authController.verifyEmail);
-router.patch("/change-password",requireSignin, authController.changePassword);
-router.post("/forgot-password", authController.forgotPassword);
-router.patch("/reset-password", authController.resetPassword);
-router.patch("/user-plan",requireSignin, authController.userPlan);
+// router.post("/verify-email", authController.verifyEmail);
+// router.patch("/change-password",requireSignin, authController.changePassword);
+// router.post("/forgot-password", authController.forgotPassword);
+// router.patch("/reset-password", authController.resetPassword);
+// router.patch("/user-plan",requireSignin, authController.userPlan);
 
 module.exports = router
